12c7a7812ae327562991271566e1b1f57f35d485
JulianSansat <juliansansat@gmail.com>
2020-03-03 21:55:14 -0300

    subcribing to broadcast channel

--- index.js
+++ index.js
@@ -10,24 +10,20 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-redis.subscribe("notifications", function(err, count) {
-  // Now we are subscribed to both the 'news' and 'music' channels.
-  // `count` represents the number of channels we are currently subscribed to.
-
-  pub.publish("notifications", "Hello world!");
-});
-
-redis.on("message", function(channel, message) {
-  // Receive message Hello world! from channel news
-  // Receive message Hello again! from channel music
-  console.log("Receive message %s from channel %s", message, channel);
-  io.emit(channel + ":" + message);
+redis.psubscribe('laravel_database_notifications', function(err, count) {
+  console.log('Subscribed');
 });
 
 io.on("connection", function(socket) {
-  socket.on("chat message", function(msg) {
-    console.log("message: " + msg);
-  });
+  console.log('A client connected');
+});
+
+redis.on('pmessage', function(subscribed, channel, data) {
+  console.log(channel);
+  data = JSON.parse(data);
+  console.log(new Date);
+  console.log(data);
+  io.emit(channel + ':' + data.event, data.data);
 });
 
 http.listen(3000, function() {
